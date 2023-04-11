6b571747920e684ed5fdf74275fb28dd4184c992
krudX <prathameshdattakumar188@gmail.com>
2023-04-11 13:20:19 +0530

    bug fixes

--- src/components/Hero/hero.component.jsx
+++ src/components/Hero/hero.component.jsx
@@ -3,24 +3,22 @@ import '../Hero/hero.styles.scss';
 
 const Hero = () => {
     return (
-        <Fragment>
-            <section className="hero">
-                <div className="hero-content">
-                    <div className="subtitle">Hey, I’m Prathamesh ✌🏼</div>
-                    <div className="title">A designer focused on creating digital products, brands and experiences.</div>
+        <section className="hero">
+            <div className="hero-content">
+                <div className="subtitle">Hey, I’m Prathamesh ✌🏼</div>
+                <div className="title">A designer focused on creating digital products, brands and experiences.</div>
 
-                    <div className="cta-container">
-                        <a href="/#" className="button button-fill">Projects</a>
-                        <a href="/#" className="button button-outline">Services</a>
-                    </div>
+                <div className="cta-container">
+                    <a href="/#projects" className="button button-fill">Projects</a>
+                    <a href="/#services" className="button button-outline">Services</a>
                 </div>
+            </div>
 
-                <div className="hero-image">
-                    <img src="https://res.cloudinary.com/krude/image/upload/v1679815650/Portfolio/faizur-rehman-yapBRdPWxik-unsplash_mgjcje.jpg" alt="hero" />
-                </div>
+            <div className="hero-image">
+                <img src="https://res.cloudinary.com/krude/image/upload/v1679815650/Portfolio/faizur-rehman-yapBRdPWxik-unsplash_mgjcje.jpg" alt="hero" />
+            </div>
 
-            </section>
-        </Fragment>
+        </section>
     )
 }
 
